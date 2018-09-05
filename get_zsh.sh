878ec810ebfff14d49260a7c0228c8b3234a620d
Paden Rose <paden.rose@q2ebanking.com>
2018-09-05 15:25:12 -0500

    Fixing some issues with the get_zsh script

--- get_zsh.sh
+++ get_zsh.sh
@@ -5,11 +5,8 @@ chsh -s /bin/zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Getting settings"
-git clone git@github.com:paden-r/env_profiles.git
-cd env_profiles
 mv .zshrc ../.zshrc
-cd ..
-source .zshrc
+source ../.zshrc
 echo "Installing Python 3"
 yum install python3
 echo "done!"
